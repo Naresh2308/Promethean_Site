3801518ba7b29c2398d9e713a5d0f6f0960ea4ee
Dhruvsingh8355 <124500232+Dhruvsingh8@users.noreply.github.com>
2023-12-10 15:36:20 +0530

    Add files via upload

---
+++ src/Constants/ImageList.js
@@ -0,0 +1,20 @@
+const ImageList = [
+  {
+    id: 1,
+    Image: 'Image/GRP-6 -Solution for effects of plastic pollution on marine e.png'
+  },
+  {
+    id: 2,
+    Image: "Image/p1.png"
+    },
+  {
+    id: 3,
+    Image: 'Image/Group 8-Technical Strategies to reduce Deforestation .png',
+  },
+  {
+    id: 4,
+    Image: 'Image/Group 10-Technical Alternatives for Fluorinated Gases used in Refrigerants.png',
+  },
+];
+
+export default ImageList;
---
+++ src/Constants/Team.js
@@ -0,0 +1,321 @@
+export const TeamInfo = [
+
+  {
+    designation: "Faculty Coordinator",
+    about:
+      "The Faculty Incharge leads the team in the right direction, disseminates and impart knowledge and assists students with applying the knowledge.",
+    id: 1,
+    member: [
+      {
+        head: [
+          {
+            name: "Dr. Smitha S kumar",
+            post: "Faculty Coordinator",
+            photo: "Image/Sumitra S Maam.jpeg",
+          },
+        ],
+        coord: [],
+      },
+    ],
+  },
+
+  {
+    designation: "CORE TEAM",
+    about:
+      "Our Core Team, which is the heart and soul of Promethean, is made up of the brightest students. It is not easy to lead a large group of people, yet they make it out to be simple.",
+    id: 2,
+    member: [
+      {
+        head: [
+          {
+            name: "Ruchika Mhethre",
+            post: "Chairperson",
+            photo: "Image/Ruchika .jpg",
+          },
+          {
+            name: "Subodh Patil",
+            post: "Secretary",
+            photo: "Image/Subodh Patil IT.jpg",
+          },
+          {
+            name: "Shreya Murthy",
+            post: "Joint Secretary",
+            photo: "Image/Shreya Murthy_Joint Secretary .jpg",
+          },
+          {
+            name: "S Parameswaran",
+            post: "Treasurer",
+            photo: "/Image/S Parameswaran_CE.jpg",
+          },
+        ],
+        coord: [],
+      },
+    ],
+  },
+  
+  {
+    designation: "TECHNICAL TEAM",
+    about:
+      "Using various technologies to create and update websites and applications. Exhibiting leadership skills and staying up to date on the latest innovations.",
+    id: 3,
+    member: [
+      {
+        head: [
+          {
+            name: "Abhishek Tiwari",
+            post: "Head",
+            photo: "Image/Abhishek Tiwari_CE.jpg",
+          },
+          {
+            name: "Gaurav Patil",
+            post: "Head",
+            photo: "Image/Gaurav_EXTC.jpg",
+          },
+          {
+            name: "Dhiraj Shelke",
+            post: "Head",
+            photo: "Image/Dhiraj Shelke(Tech Head).jpg",
+          },
+        ],
+        coord: [
+          {
+            name: "Avaneesh Karthikeyan Iyer",
+            post: "Coordinator",
+            photo: "Image/Avaneesh Karthikeyan Technical _coord.jpg",
+          },
+          {
+            name: "Abinaya Jeyapandi",
+            post: "Coordinator",
+            photo: "Image/Abinaya_Techcoord.jpg",
+          },
+          {
+            name: "Avishkar Phawade",
+            post: "Coordinator",
+            photo: "Image/Avishkar Phawade_Tech.jpg",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    designation: "DESIGN TEAM",
+    about:
+      "Creating innovative posts that will amaze everyone with their vibrant, appealing, and instructive content. They are creative and hardworking individuals that have a real interest in and ability for design.",
+    id: 4,
+    member: [
+      {
+        head: [
+          {
+            name: "Shriya Kolambkar",
+            post: "Head",
+            photo: "Image/ShriyaKolambkar_Design_Head.JPG",
+          },
+          {
+            name: "Varad Thorat",
+            post: "Head",
+            photo: "Image/Varad Thorat Design head.jpg",
+          },
+        ],
+        coord: [
+          {
+            name: "Harshvinder Kaur Bal",
+            post: "Coordinator",
+            photo: "Image/Harshvinder Kaur Bal.jpeg",
+          },
+          {
+            name: "Devdatta Temgire",
+            post: "Coordinator",
+            photo: "Image/DEVDATTA_TEMGIRE_DESIGN.jpg",
+          },
+          {
+            name: "Preksha Siyal",
+            post: "Coordinator",
+            photo: "Image/Preksha_Siyal_Design_Co-ord_.jpg",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    designation: "MARKETING TEAM",
+    about:
+      "The Marketing team makes the events a tremendous success by recruiting sponsors and pushing students to participate as much as possible.",
+    id: 5,
+    member: [
+      {
+        head: [
+          {
+            name: "Kritika Pandey",
+            post: "Head",
+            photo: "Image/Kritika_Pandey_.jpg",
+          },
+          {
+            name: "Sarika Hire",
+            post: "Head",
+            photo: "Image/Sarika Hire.jpg",
+          },
+          {
+            name: "Siddhant Singh",
+            post: "Head",
+            photo: "Image/Siddhant Singh.jpg",
+          },
+        ],
+        coord: [
+          {
+            name: "Abhinav Tiwari",
+            post: "Coordinator",
+            photo: "Image/Abhinav Tiwari_marketing co-ord.png",
+          },
+          {
+            name: "Samyak Randive",
+            post: "Coordinator",
+            photo: "Image/Samyak Randive _Marketing_.jpg",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    designation: "PUBLIC RELATION TEAM",
+    about:
+      "It is solely responsible for all external communication from the institute. It aims to position and popularize the Promethean on a variety of platforms.",
+    id: 6,
+    member: [
+      {
+        head: [
+          {
+            name: "Nidhi Kulkarni",
+            post: "Head",
+            photo: "Image/Nidhi Kulkarni_EXTC.jpeg",
+          },
+          {
+            name: "Sohamm Swamy",
+            post: "Head",
+            photo: "Image/Sohamm Swammy CE_ccexpress.jpeg",
+          },
+        ],
+        coord: [
+          {
+            name: "Nandana Anil",
+            post: "Coordinator",
+            photo: "Image/Nandana Anil_PR.jpg",
+          },
+          {
+            name: "Shreyoshi Roy",
+            post: "Coordinator",
+            photo: "Image/SHREYOSHI ROY PR.jpg",
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    designation: "PUBLICITY TEAM",
+    about:
+      "The team is in charge of gathering, illustrating, and disseminating important information to students.",
+    id: 7,
+    member: [
+      {
+        head: [
+          {
+            name: "Aayush Bhatnagar",
+            post: "Head",
+            photo: "Image/Aayush Bhatnagar.JPG",
+          },
+          {
+            name: "Mansi Kore",
+            post: "Head",
+            photo: "Image/Mansi Kore_Publicity head.jpeg",
+          },
+          {
+            name: "Sahil Wake",
+            post: "Head",
+            photo: "Image/Sahil Wake_CE.jpg",
+          },
+        ],
+        coord: [
+          {
+            name: "Alisha Fatima",
+            post: "Coordinator",
+            photo: "Image/Alisha Fatima_Publicity.jpg",
+          },
+          {
+            name: "Aryan Chalke",
+            post: "Coordinator",
+            photo: "Image/Aryan Chalke _ Publicity .jpg",
+          },
+          {
+            name: "Lokeshwar Nelvai",
+            post: "Coordinator",
+            photo: "Image/Lokeshwar Nelvai_Publicity.jpeg",
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    designation: "CREATIVE TEAM",
+    about:
+      "They have established their benchmark by working together to achieve a specific goal. Using their creativity, they make the events as appealing as possible. ",
+    id: 8,
+    member: [
+      {
+        head: [
+          {
+            name: "Anurag Singh",
+            post: "Head",
+            photo: "Image/Anurag Creative.jpeg",
+          },
+          {
+            name: "Kruti Upasani",
+            post: "Head",
+            photo: "Image/Kruti Upasani_IT.jpg",
+          },
+        ],
+        coord: [
+          {
+            name: "Lavanya Kurup ",
+            post: "Coordinator",
+            photo: "Image/Lavanya kurup creative_.jpg",
+          },
+          {
+            name: "Rushikesh Joshi",
+            post: "Coordinator",
+            photo: "Image/Rushikesh Joshi _ Creative team_1.jpg",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    designation: "SOCIAL MEDIA TEAM",
+    about:
+      "Creating visually appealing posts with well-written descriptions. Enhancing Promethean’s  internet presence through promoting activities and keeping students updated about contemporary affairs.",
+    id: 9,
+    member: [
+      {
+        head: [
+          {
+            name: "Saurabh Shinde",
+            post: "Head",
+            photo: "Image/Saurabh Shinde.jpeg",
+          },
+        ],
+        coord: [
+          {
+            name: "Soham Wagh",
+            post: "Coordinator",
+            photo: "Image/Soham Wagh_.jpg",
+          },
+          {
+            name: "Sujal Gaikwad ",
+            post: "Coordinator",
+            photo: "Image/Sujal Gaikwad .jpg",
+          },
+        ],
+      },
+    ],
+  },
+];
