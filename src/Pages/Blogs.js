929744ada15fdb6bb7fe1a8679c7651b107e88bc
Naresh2308 <114303154+Naresh2308@users.noreply.github.com>
2023-12-27 23:36:48 +0530

    Update Blogs.js
    added section cards with fixed images and fixed size

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -23,13 +23,9 @@ const Main = styled.div`
 `;
 
 const AboutImage = styled.img`
-  padding:10px;
-  max-height:250px;
-  min-height:200px;
-  border-radius:40px;
-  @media ${(props) => props.theme.MediaQueries.l.query} {
-    max-height: 250px;
-  }
+  height:251px;
+  width:375px;
+  border-radius:5px;
 `;
 
 
@@ -50,16 +46,11 @@ const Header = styled.h1`
 const Title = styled.h1`
   font-family: ${(props) => props.theme.Fonts.Josefin};
   font-weight: 600;
-  font-size: 18px;
-  text-align:center;
+  font-size: 24px;
+  text-align:left;
+  padding:10px;
   color: ${(props) => props.theme.Colors.Header};
-  margin-bottom: 20px;
-  @media ${(props) => props.theme.MediaQueries.m.query} {
-    font-size: 20px;
-  }
-  @media ${(props) => props.theme.MediaQueries.l.query} {
-    font-size: 24px;
-  }
+  
 `;
 
 const HeroDiv = styled.div`
@@ -87,19 +78,30 @@ justify-content:center;
 `;
 
 const Section = styled.div`
-max-width: 100%;
 display:flex;
-margin:10px;
-border-radius:20px;
+margin:12px;
+border-radius:15px;
 flex-wrap:wrap;
-max-width:250px;
-background-color:rgba(210, 217, 178, 0.4);
-
+width:375px;
+height:500px;
+background-color:rgb(11, 29, 39,0.5);
+border:2px solid #FFFFFF70;
+padding:0px;
 justify-content:center;
 &:hover {
       transform: scale(1.1);
     }
 `;
+
+const Para=styled.p`
+  font-family: ${(props) => props.theme.Fonts.Josefin};
+  font-weight: 300;
+  font-size: 20px;
+  text-align:left;
+  padding:10px;
+  color: ${(props) => props.theme.Colors.Header};
+`;
+
 
 const Blogs = () => {
   return (
@@ -115,87 +117,87 @@ const Blogs = () => {
         <Blogging>
         <Section>
         <Link to={ROUTES.BLOG_ONE}>
-            <a href={ROUTES.BLOG_ONE}><br/><AboutImage src={Blog1}/><br/><Title>Advancing Towards Net-Zero</Title><br/></a>
+            <a href={ROUTES.BLOG_ONE}><br/><AboutImage src={Blog1}/><br/><Title>Advancing Towards Net-Zero</Title><br/><Para>In the quest for a sustainable future, the urgency to address climate change has never been more pronounced. The Intergovernmental Panel on Climate Change (IPCC) emphasized in its 2022...</Para></a>
           </Link>
         </Section>
          <Section>
          <Link to={ROUTES.BLOG_TWO}>
-            <a href={ROUTES.BLOG_TWO}><br/><AboutImage src={Blog2}/><br/><br/><Title>Tech Innovations Powering Environmental Sustainability</Title></a>
+            <a href={ROUTES.BLOG_TWO}><br/><AboutImage src={Blog2}/><br/><Title>Tech Innovations Powering Environmental Sustainability</Title><br/><Para>In an era marked by rapid technological advancements, the intersection of technology and environmental conservation has become increasingly crucial. Innovations...</Para></a>
           </Link>
          </Section>
          <Section>
          <Link to={ROUTES.BLOG_THREE}>
-            <a href={ROUTES.BLOG_THREE}><br/><AboutImage src={Blog3}/><br/><br/><Title>Exploring the Impact of Renewable Energy Technologies</Title></a>
+            <a href={ROUTES.BLOG_THREE}><br/><AboutImage src={Blog3}/><br/><Title>Exploring the Impact of Renewable Energy Technologies</Title><br/><Para>The world is facing a significant challenge in combating climate change and reducing reliance on fossil fuels. Renewable energy technologies offer a promising...</Para></a>
           </Link>
          </Section>
          <Section>
           <Link to={ROUTES.BLOG_FOUR}>
-            <a href={ROUTES.BLOG_FOUR}><br/><AboutImage src={Blog4}/><br/><br/><Title>Revolutionizing Plastics</Title></a>
+            <a href={ROUTES.BLOG_FOUR}><br/><AboutImage src={Blog4}/><br/><Title>Revolutionizing Plastics</Title><br/><Para>In a world where the environmental impact of plastic waste is a growing concern, the search for eco-friendly alternatives has become more imperative than ever. One promising solution is the ...</Para></a>
           </Link>
           </Section>
           <Section>
           <Link to={ROUTES.BLOG_FIVE}>
-            <a href={ROUTES.BLOG_FIVE}><br/><AboutImage src={Blog5}/><br/><br/><Title>The Environmental Toll of Car-Centric Infrastructure</Title></a>
+            <a href={ROUTES.BLOG_FIVE}><br/><AboutImage src={Blog5}/><br/><Title>The Environmental Toll of Car-Centric Infrastructure</Title><br/><Para>In the relentless pursuit of modernization and convenience, many cities worldwide have fallen victim to the pitfalls of car-centric infrastructure. While automobiles undoubtedly supply...</Para></a>
           </Link>
           </Section>
           <Section>          
           <Link to={ROUTES.BLOG_SIX}>
-            <a href={ROUTES.BLOG_SIX}><br/><AboutImage src={Blog6}/><br/><br/><Title>The Environmental Impacts of Rivers</Title></a>
+            <a href={ROUTES.BLOG_SIX}><br/><AboutImage src={Blog6}/><br/><Title>The Environmental Impacts of Rivers</Title><br/><Para>Rivers, often considered the arteries of our planet, play a crucial role in shaping landscapes and sustaining life. However, their immense importance is...</Para></a>
           </Link>
           </Section>
           <Section>
           <Link to={ROUTES.BLOG_SEVEN}>
-            <a href={ROUTES.BLOG_SEVEN}><br/><AboutImage src={Blog7}/><br/><br/><Title>Embracing Green Havens</Title></a>
+            <a href={ROUTES.BLOG_SEVEN}><br/><AboutImage src={Blog7}/><br/><Title>Embracing Green Havens</Title><br/><Para>In the hustle and bustle of modern city life, amidst towering skyscrapers and bustling streets, there exists an often overlooked oasis that plays a crucial role ...</Para></a>
           </Link>
           </Section>
           <Section>
           <Link to={ROUTES.BLOG_EIGHT}>
-            <a href={ROUTES.BLOG_EIGHT}><br/><AboutImage/><br/><br/></a>
+            <a href={ROUTES.BLOG_EIGHT}><br/><AboutImage/><br/><br/><Para></Para></a>
           </Link>
           </Section>
           <Section>
           <Link to={ROUTES.BLOG_NINE}>
-            <a href={ROUTES.BLOG_NINE}><br/><AboutImage/><br/><br/></a>
+            <a href={ROUTES.BLOG_NINE}><br/><AboutImage/><br/><br/><Para></Para></a>
           </Link>
           </Section>
           <Section>
           <Link to={ROUTES.BLOG_TEN}>
-            <a href={ROUTES.BLOG_TEN}><br/><AboutImage/><br/><br/></a>
+            <a href={ROUTES.BLOG_TEN}><br/><AboutImage/><br/><br/><Para></Para></a>
           </Link>
           </Section>
           <Section>
           <Link to={ROUTES.BLOG_ELEVEN}>
-            <a href={ROUTES.BLOG_ELEVEN}><br/><AboutImage/><br/><br/></a>
+            <a href={ROUTES.BLOG_ELEVEN}><br/><AboutImage/><br/><br/><Para></Para></a>
           </Link>
           </Section>
           <Section>
           <Link to={ROUTES.BLOG_TWELVE}>
-            <a href={ROUTES.BLOG_TWELVE}><br/><AboutImage/><br/><br/></a>
+            <a href={ROUTES.BLOG_TWELVE}><br/><AboutImage/><br/><br/><Para></Para></a>
           </Link>
           </Section>
           <Section>
           <Link to={ROUTES.BLOG_THIRTEEN}>
-            <a href={ROUTES.BLOG_THIRTEEN}><br/><AboutImage/><br/><br/></a>
+            <a href={ROUTES.BLOG_THIRTEEN}><br/><AboutImage/><br/><br/><Para></Para></a>
           </Link>
           </Section>
           <Section>
           <Link to={ROUTES.BLOG_FOURTEEN}>
-            <a href={ROUTES.BLOG_FOURTEEN}><br/><AboutImage/><br/><br/></a>
+            <a href={ROUTES.BLOG_FOURTEEN}><br/><AboutImage/><br/><br/><Para></Para></a>
           </Link>
           </Section>
           <Section>
           <Link to={ROUTES.BLOG_FIFTEEN}>
-            <a href={ROUTES.BLOG_FIFTEEN}><br/><AboutImage/><br/><br/></a>
+            <a href={ROUTES.BLOG_FIFTEEN}><br/><AboutImage/><br/><br/><Para></Para></a>
           </Link>
           </Section>
           <Section>
           <Link to={ROUTES.BLOG_SIXTEEN}>
-            <a href={ROUTES.BLOG_SIXTEEN}><br/><AboutImage/><br/><br/></a>
+            <a href={ROUTES.BLOG_SIXTEEN}><br/><AboutImage/><br/><br/><Para></Para></a>
           </Link>
           </Section>
           <Section>
           <Link to={ROUTES.BLOG_SEVENTEEN}>
-            <a href={ROUTES.BLOG_SEVENTEEN}><br/><AboutImage/><br/><br/></a>
+            <a href={ROUTES.BLOG_SEVENTEEN}><br/><AboutImage/><br/><br/><Para></Para></a>
           </Link>
           </Section>
           </Blogging>
