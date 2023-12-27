c817d612ed38df5bf027f4641470e828de78a902
Omkarraut18 <147294172+Omkarraut18@users.noreply.github.com>
2023-12-27 21:55:03 +0530

    Update blog9.js

--- src/Pages/Blogssample/blog9.js
+++ src/Pages/Blogssample/blog9.js
@@ -1,6 +1,7 @@
 import react from 'react';
 import styled from "styled-components";
 import React from 'react';
+import HeroImg from "../Blogssample/heroimg.jpg";
 
 const Main = styled.div`
   max-width: 320px;
@@ -28,12 +29,17 @@ const Header = styled.h1`
 
 const HeroDiv = styled.div`
   max-width: 100%;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
   position: relative;
   height: 50vh;
   justify-content:center;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1);
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   padding: 20px;
   text-align: center;
 `;
@@ -41,16 +47,22 @@ const HeroDiv = styled.div`
 const Blogging = styled.div`
 max-width: 100%;
 font-size: 25px;
+padding:60px;
+margin:45px;
 text-align:center;
+border-radius:30px;
 font-family: ${(props) => props.theme.Fonts.Josefin};
 background-color: rgba(255, 255, 255, 0.7);
-cursor: pointer;
 `;
 
 const BLOG_NINE = () => {
     return (
         <>
-          <HeroDiv>
+          <HeroDiv
+           style={{
+             backgroundImage: ` linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)) , url(${HeroImg})`,
+           }}
+          >
             <Header>Blogs9 Name</Header>
           </HeroDiv>
             <Main>
