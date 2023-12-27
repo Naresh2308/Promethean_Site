6d134e5506a2bb867882b5e02b29a3b458d9d411
Omkarraut18 <147294172+Omkarraut18@users.noreply.github.com>
2023-12-27 21:35:32 +0530

    Update Blogs.js

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -23,19 +23,12 @@ const Main = styled.div`
 `;
 
 const AboutImage = styled.img`
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  border-radius: 10px;
-  width: 250px;
-  height: 250px;
+  padding:10px;
+  max-height:250px;
+  min-height:200px;
+  border-radius:40px;
   @media ${(props) => props.theme.MediaQueries.l.query} {
-    margin-right: 40px;
-    width: 350px;
-    height: 350px;
-  }
-  @media ${(props) => props.theme.MediaQueries.xl.query} {
-    margin: ${(props) => (props.right ? "0 120px 0 0" : "0 0 0 80px")};
+    max-height: 250px;
   }
 `;
 
@@ -51,6 +44,21 @@ const Header = styled.h1`
   }
   @media ${(props) => props.theme.MediaQueries.l.query} {
     font-size: 90px;
+  }
+`;
+
+const Title = styled.h1`
+  font-family: ${(props) => props.theme.Fonts.Josefin};
+  font-weight: 600;
+  font-size: 18px;
+  text-align:center;
+  color: ${(props) => props.theme.Colors.Header};
+  margin-bottom: 20px;
+  @media ${(props) => props.theme.MediaQueries.m.query} {
+    font-size: 20px;
+  }
+  @media ${(props) => props.theme.MediaQueries.l.query} {
+    font-size: 24px;
   }
 `;
 
@@ -73,12 +81,26 @@ const HeroDiv = styled.div`
 
 const Blogging = styled.div`
 max-width: 100%;
-font-size: 20px;
-text-align: center;
-font-family: ${(props) => props.theme.Fonts.Josefin};
-background-color: rgba(255, 255, 255, 0.7);
-cursor: pointer;
+display:flex;
+flex-wrap:wrap;
+justify-content:center;
 `;
+
+const Section = styled.div`
+max-width: 100%;
+display:flex;
+margin:10px;
+border-radius:20px;
+flex-wrap:wrap;
+max-width:250px;
+background-color:rgba(210, 217, 178, 0.4);
+
+justify-content:center;
+&:hover {
+      transform: scale(1.1);
+    }
+`;
+
 const Blogs = () => {
   return (
     <>
@@ -91,57 +113,91 @@ const Blogs = () => {
       </HeroDiv>
         <Main>
         <Blogging>
+        <Section>
         <Link to={ROUTES.BLOG_ONE}>
-            <a href={ROUTES.BLOG_ONE}><br/><AboutImage src={Blog1}/><br/><br/></a>
+            <a href={ROUTES.BLOG_ONE}><br/><AboutImage src={Blog1}/><br/><Title>Advancing Towards Net-Zero</Title><br/></a>
           </Link>
-          <Link to={ROUTES.BLOG_TWO}>
-            <a href={ROUTES.BLOG_TWO}><br/><AboutImage src={Blog2}/><br/><br/></a>
+        </Section>
+         <Section>
+         <Link to={ROUTES.BLOG_TWO}>
+            <a href={ROUTES.BLOG_TWO}><br/><AboutImage src={Blog2}/><br/><br/><Title>Tech Innovations Powering Environmental Sustainability</Title></a>
           </Link>
-          <Link to={ROUTES.BLOG_THREE}>
-            <a href={ROUTES.BLOG_THREE}><br/><AboutImage src={Blog3}/><br/><br/></a>
+         </Section>
+         <Section>
+         <Link to={ROUTES.BLOG_THREE}>
+            <a href={ROUTES.BLOG_THREE}><br/><AboutImage src={Blog3}/><br/><br/><Title>Exploring the Impact of Renewable Energy Technologies</Title></a>
           </Link>
+         </Section>
+         <Section>
           <Link to={ROUTES.BLOG_FOUR}>
-            <a href={ROUTES.BLOG_FOUR}><br/><AboutImage src={Blog4}/><br/><br/></a>
+            <a href={ROUTES.BLOG_FOUR}><br/><AboutImage src={Blog4}/><br/><br/><Title>Revolutionizing Plastics</Title></a>
           </Link>
+          </Section>
+          <Section>
           <Link to={ROUTES.BLOG_FIVE}>
-            <a href={ROUTES.BLOG_FIVE}><br/><AboutImage src={Blog5}/><br/><br/></a>
+            <a href={ROUTES.BLOG_FIVE}><br/><AboutImage src={Blog5}/><br/><br/><Title>The Environmental Toll of Car-Centric Infrastructure</Title></a>
           </Link>
+          </Section>
+          <Section>          
           <Link to={ROUTES.BLOG_SIX}>
-            <a href={ROUTES.BLOG_SIX}><br/><AboutImage src={Blog6}/><br/><br/></a>
+            <a href={ROUTES.BLOG_SIX}><br/><AboutImage src={Blog6}/><br/><br/><Title>The Environmental Impacts of Rivers</Title></a>
           </Link>
+          </Section>
+          <Section>
           <Link to={ROUTES.BLOG_SEVEN}>
-            <a href={ROUTES.BLOG_SEVEN}><br/><AboutImage src={Blog7}/><br/><br/></a>
+            <a href={ROUTES.BLOG_SEVEN}><br/><AboutImage src={Blog7}/><br/><br/><Title>Embracing Green Havens</Title></a>
           </Link>
+          </Section>
+          <Section>
           <Link to={ROUTES.BLOG_EIGHT}>
-            <a href={ROUTES.BLOG_EIGHT}><br/><AboutImage src={PrometheanImage}/><br/><br/></a>
+            <a href={ROUTES.BLOG_EIGHT}><br/><AboutImage/><br/><br/></a>
           </Link>
+          </Section>
+          <Section>
           <Link to={ROUTES.BLOG_NINE}>
-            <a href={ROUTES.BLOG_NINE}><br/><AboutImage src={PrometheanImage}/><br/><br/></a>
+            <a href={ROUTES.BLOG_NINE}><br/><AboutImage/><br/><br/></a>
           </Link>
+          </Section>
+          <Section>
           <Link to={ROUTES.BLOG_TEN}>
-            <a href={ROUTES.BLOG_TEN}><br/><AboutImage src={PrometheanImage}/><br/><br/></a>
+            <a href={ROUTES.BLOG_TEN}><br/><AboutImage/><br/><br/></a>
           </Link>
+          </Section>
+          <Section>
           <Link to={ROUTES.BLOG_ELEVEN}>
-            <a href={ROUTES.BLOG_ELEVEN}><br/><AboutImage src={PrometheanImage}/><br/><br/></a>
+            <a href={ROUTES.BLOG_ELEVEN}><br/><AboutImage/><br/><br/></a>
           </Link>
+          </Section>
+          <Section>
           <Link to={ROUTES.BLOG_TWELVE}>
-            <a href={ROUTES.BLOG_TWELVE}><br/><AboutImage src={PrometheanImage}/><br/><br/></a>
+            <a href={ROUTES.BLOG_TWELVE}><br/><AboutImage/><br/><br/></a>
           </Link>
+          </Section>
+          <Section>
           <Link to={ROUTES.BLOG_THIRTEEN}>
-            <a href={ROUTES.BLOG_THIRTEEN}><br/><AboutImage src={PrometheanImage}/><br/><br/></a>
+            <a href={ROUTES.BLOG_THIRTEEN}><br/><AboutImage/><br/><br/></a>
           </Link>
+          </Section>
+          <Section>
           <Link to={ROUTES.BLOG_FOURTEEN}>
-            <a href={ROUTES.BLOG_FOURTEEN}><br/><AboutImage src={PrometheanImage}/><br/><br/></a>
+            <a href={ROUTES.BLOG_FOURTEEN}><br/><AboutImage/><br/><br/></a>
           </Link>
+          </Section>
+          <Section>
           <Link to={ROUTES.BLOG_FIFTEEN}>
-            <a href={ROUTES.BLOG_FIFTEEN}><br/><AboutImage src={PrometheanImage}/><br/><br/></a>
+            <a href={ROUTES.BLOG_FIFTEEN}><br/><AboutImage/><br/><br/></a>
           </Link>
+          </Section>
+          <Section>
           <Link to={ROUTES.BLOG_SIXTEEN}>
-            <a href={ROUTES.BLOG_SIXTEEN}><br/><AboutImage src={PrometheanImage}/><br/><br/></a>
+            <a href={ROUTES.BLOG_SIXTEEN}><br/><AboutImage/><br/><br/></a>
           </Link>
+          </Section>
+          <Section>
           <Link to={ROUTES.BLOG_SEVENTEEN}>
-            <a href={ROUTES.BLOG_SEVENTEEN}><br/><AboutImage src={PrometheanImage}/><br/><br/></a>
+            <a href={ROUTES.BLOG_SEVENTEEN}><br/><AboutImage/><br/><br/></a>
           </Link>
+          </Section>
           </Blogging>
         </Main>
     </>
