7f3feeb1bd3011cf26929de77de85e83f1139cb3
Dhruvsingh8355 <124500232+Dhruvsingh8@users.noreply.github.com>
2023-12-30 19:55:50 +0530

    Update Blogs.js

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import HeroImg from "../Images/heroimg.jpg";
+import HeroImg from "./Blogssample/heroimgblog/title.webp";
 import React from 'react';
 import * as ROUTES from "../Constants/routes";
 import { Link } from "react-router-dom";
@@ -10,7 +10,8 @@ import Blog4 from "../Images/Blog4.jpeg";
 import Blog5 from "../Images/Blog5.webp";
 import Blog6 from "../Images/Blog6.jpeg";
 import Blog7 from "../Images/Blog7.jpeg";
-import Blog8 from "../Images/Blog8.jpeg";
+import Blog8 from "../Images/Blog8.webp";
+import Blog9 from "../Images/Blog9.webp";
 
 const Main = styled.div`
   max-width: 320px;
@@ -129,50 +130,50 @@ const Blogs = () => {
         <Blogging>
         <Section>
         <Link to={ROUTES.BLOG_ONE}>
-            <a href={ROUTES.BLOG_ONE}><br/><AboutImage src={Blog1}/><br/><Title>Advancing Towards Net-Zero</Title><Para>In the quest for a sustainable future, the urgency to address climate change has never been more pronounced. The Intergovernmental Panel on Climate Change (IPCC) emphasized in its 2022 report on the Mitigation of Climate...</Para> <Read>Read more</Read></a>
+            <a href={ROUTES.BLOG_ONE}><br/><AboutImage src={Blog1}/><br/><Title>Advancing Towards Net-Zero</Title><Para>In the quest for a sustainable future, the urgency to address climate change has never been more pronounced. The Intergovernmental Panel on Climate Change (IPCC) emphasized in its 2022 report on the Mitigation of Climate...</Para><Read>Read more</Read></a>
           </Link>
         </Section>
          <Section>
          <Link to={ROUTES.BLOG_TWO}>
-            <a href={ROUTES.BLOG_TWO}><br/><AboutImage src={Blog2}/><br/><Title>Tech Innovations Powering Environmental Sustainability</Title><Para>In an era marked by rapid technological advancements, the intersection of technology and environmental conservation has become increasingly crucial. Innovations...</Para> <Read>Read more</Read></a>
+            <a href={ROUTES.BLOG_TWO}><br/><AboutImage src={Blog2}/><br/><Title>Tech Innovations Powering Environmental Sustainability</Title><Para>In an era marked by rapid technological advancements, the intersection of technology and environmental conservation has become increasingly crucial. Innovations...</Para><Read>Read more</Read></a>
           </Link>
          </Section>
          <Section>
          <Link to={ROUTES.BLOG_THREE}>
-            <a href={ROUTES.BLOG_THREE}><br/><AboutImage src={Blog3}/><br/><Title>Exploring the Impact of Ren-ewable Energy Technologies</Title><Para>The world is facing a significant challenge in combating climate change and reducing reliance on fossil fuels. Renewable energy technologies offer a promising...</Para> <Read>Read more</Read></a>
+            <a href={ROUTES.BLOG_THREE}><br/><AboutImage src={Blog3}/><br/><Title>Exploring the Impact of Ren-ewable Energy Technologies</Title><Para>The world is facing a significant challenge in combating climate change and reducing reliance on fossil fuels. Renewable energy technologies offer a promising...</Para><Read>Read more</Read></a>
           </Link>
          </Section>
          <Section>
           <Link to={ROUTES.BLOG_FOUR}>
-            <a href={ROUTES.BLOG_FOUR}><br/><AboutImage src={Blog4}/><br/><Title>Revolutionizing Plastics</Title><Para>In a world where the environmental impact of plastic waste is a growing concern, the search for eco-friendly alternatives has become more imperative than ever. One promising solution is the use of starch in the production...</Para> <Read>Read more</Read></a>
+            <a href={ROUTES.BLOG_FOUR}><br/><AboutImage src={Blog4}/><br/><Title>Revolutionizing Plastics</Title><Para>In a world where the environmental impact of plastic waste is a growing concern, the search for eco-friendly alternatives has become more imperative than ever. One promising solution is the use of starch in the production...</Para><Read>Read more</Read></a>
           </Link>
           </Section>
           <Section>
           <Link to={ROUTES.BLOG_FIVE}>
-            <a href={ROUTES.BLOG_FIVE}><br/><AboutImage src={Blog5}/><br/><Title>The Environmental Toll of Car-Centric Infrastructure</Title><Para>In the relentless pursuit of modernization and convenience, many cities worldwide have fallen victim to the pitfalls of car-centric infrastructure. While automobiles undoubtedly supply...</Para> <Read>Read more</Read></a>
+            <a href={ROUTES.BLOG_FIVE}><br/><AboutImage src={Blog5}/><br/><Title>The Environmental Toll of Car-Centric Infrastructure</Title><Para>In the relentless pursuit of modernization and convenience, many cities worldwide have fallen victim to the pitfalls of car-centric infrastructure. While automobiles undoubtedly supply...</Para><Read>Read more</Read></a>
           </Link>
           </Section>
           <Section>          
           <Link to={ROUTES.BLOG_SIX}>
-            <a href={ROUTES.BLOG_SIX}><br/><AboutImage src={Blog6}/><br/><Title>The Environmental Impacts of Rivers</Title><Para>Rivers, often considered the arteries of our planet, play a crucial role in shaping landscapes and sustaining life. However, their immense importance is acco-mpanied by a range...</Para> <Read>Read more</Read></a>
+            <a href={ROUTES.BLOG_SIX}><br/><AboutImage src={Blog6}/><br/><Title>The Environmental Impacts of Rivers</Title><Para>Rivers, often considered the arteries of our planet, play a crucial role in shaping landscapes and sustaining life. However, their immense importance is acco-mpanied by a range...</Para><Read>Read more</Read></a>
           </Link>
           </Section>
           <Section>
           <Link to={ROUTES.BLOG_SEVEN}>
-            <a href={ROUTES.BLOG_SEVEN}><br/><AboutImage src={Blog7}/><br/><Title>Embracing Green Havens</Title><Para>In the hustle and bustle of modern city life, amidst towering skyscrapers and bustling streets, there exists an often overlooked oasis that plays a crucial role in our well-being — urban green spaces. Parks, gardens, and green areas are...</Para> <Read>Read more</Read></a>
+            <a href={ROUTES.BLOG_SEVEN}><br/><AboutImage src={Blog7}/><br/><Title>Embracing Green Havens</Title><Para>In the hustle and bustle of modern city life, amidst towering skyscrapers and bustling streets, there exists an often overlooked oasis that plays a crucial role in our well-being — urban green spaces. Parks, gardens, and green areas are...</Para><Read>Read more</Read></a>
           </Link>
           </Section>
-          <Section>
+           <Section>
           <Link to={ROUTES.BLOG_EIGHT}>
-            <a href={ROUTES.BLOG_EIGHT}><br/><AboutImage src={Blog8}/><br/><Title>Reviving Urban Biodiversity</Title><Para>In the bustling heart of urban landscapes, amidst the towering structures and bustling streets, lies a silent casualty: the decline of urban biodiversity. As cities expand and concrete conquers once-thriving...</Para><Read>Read more</Read></a>
-          </Link>
-          </Section>
-          {/* <Section>
-          <Link to={ROUTES.BLOG_NINE}>
-            <a href={ROUTES.BLOG_NINE}><br/><AboutImage/><br/><Para></Para><Read>Read more</Read></a>
+            <a href={ROUTES.BLOG_EIGHT}><br/><AboutImage src={Blog8}/><br/><Title>The Power of Regenerative Agriculture in Restoring our Environment</Title><Para>In recent years, there has been a growing concern about the state of our environment and the urgent need for sustainable solutions.One such solution...</Para><Read>Read more</Read></a>
           </Link>
           </Section>
           <Section>
+          <Link to={ROUTES.BLOG_NINE}>
+            <a href={ROUTES.BLOG_NINE}><br/><AboutImage src={Blog9}/><br/><Title>The Rise of Vertical Forests: A Green</Title><Para>As we witness the rapid urbanization and the ever-increasing population in cities, the need for sustainable and eco-friendly solutions becomes more pressing than ever. One innovative concept that has...</Para><Read>Read more</Read></a>
+          </Link>
+          </Section>
+          {/*<Section>
           <Link to={ROUTES.BLOG_TEN}>
             <a href={ROUTES.BLOG_TEN}><br/><AboutImage/><br/><Para></Para><Read>Read more</Read></a>
           </Link>
